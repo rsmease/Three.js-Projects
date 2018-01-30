cc40ec6555b7dfd63a1d647c67fcc9843f79da07
Ryan Mease <rsmease@gmail.com>
2018-01-30 09:59:48 -0800

    Begin work, exercise with GUI interaction

--- archive/archive4.js
+++ archive/archive4.js
@@ -26,22 +26,19 @@ renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 //plane
-const planeGeometry = new THREE.PlaneGeometry(60, 20, 1, 1);
+const planeGeometry = new THREE.PlaneGeometry(30, 30, 1, 1);
 const planeMaterial = new THREE.MeshLambertMaterial({
-    color: "white"
+    color: "green",
+    side: THREE.DoubleSide
 });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.receiveShadow = true;
 
-// rotate and position the plane
-plane.rotation.x = -0.5 * Math.PI;
-plane.position.x = 15;
-plane.position.y = 0;
-plane.position.z = 0;
+plane.rotation.x = 1.4;
 
 scene.add(plane);
 
-// create a cube
+//cube
 const cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
 const cubeMaterial = new THREE.MeshLambertMaterial({
     color: "blue"
@@ -49,49 +46,15 @@ const cubeMaterial = new THREE.MeshLambertMaterial({
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 cube.castShadow = true;
 
-// position the cube
-cube.position.x = -4;
-cube.position.y = 3;
-cube.position.z = 0;
 
-// add the cube to the scene
 scene.add(cube);
 
-const sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-const sphereMaterial = new THREE.MeshLambertMaterial({
-    color: 0x7777ff
-});
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-sphere.castShadow = true;
-
-// position the sphere
-sphere.position.x = 20;
-sphere.position.y = 0;
-sphere.position.z = 2;
-sphere.castShadow = true;
-
-// add the sphere to the scene
-scene.add(sphere);
-
-// add subtle ambient lighting
-var ambientLight = new THREE.AmbientLight("green");
-scene.add(ambientLight);
-
-// add spotlight for the shadows
-var spotLight = new THREE.SpotLight(0xffffff);
+//spotlight
+const spotLight = new THREE.SpotLight("white");
 spotLight.position.set(-40, 60, -10);
 spotLight.castShadow = true;
 scene.add(spotLight);
 
-//gui
-var controls = new function () {
-    this.rotationSpeed = 0.02;
-    this.bouncingSpeed = 0.03;
-};
-
-var gui = new dat.GUI();
-gui.add(controls, 'rotationSpeed', 0, 0.5);
-gui.add(controls, 'bouncingSpeed', 0, 0.5);
 
 const initStats = function () {
 
@@ -112,17 +75,16 @@ const initStats = function () {
 //stats
 const displayStats = initStats();
 
-
 let step = 0;
 const animate = function () {
     displayStats.update();
 
 
-    // cube.rotation.y += 0.01;
+    cube.rotation.y += 0.01;
 
-    // step += 0.04;
-    // cube.position.x = 2 + (10 * (Math.cos(step)));
-    // cube.position.y = 2 + (10 * Math.abs(Math.sin(step)));
+    step += 0.04;
+    cube.position.x = 2 + (10 * (Math.cos(step)));
+    cube.position.y = 2 + (10 * Math.abs(Math.sin(step)));
 
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
